fa56c58032fabff8a85be15f7381865abdc96787
Juan I. Frank <juanfrank.dev@gmail.com>
2024-12-31 00:08:38 -0300

    feat: connect with local mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, MongooseModule.forRoot('mongodb://localhost:27017')],
   controllers: [],
   providers: [],
+
 })
 export class AppModule {}
