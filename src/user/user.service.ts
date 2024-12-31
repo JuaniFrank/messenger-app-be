79386dd06c3af8d5a79f8051e714388879913bdd
Juan I. Frank <juanfrank.dev@gmail.com>
2024-12-31 00:54:54 -0300

    feat: user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from 'schemas/user.schema';
+import { Model, ObjectId } from 'mongoose';
+import { ValidateIdDto } from './dto/validate-id.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto) {
+
+    const emailExists = await this.userModel.findOne({ email: createUserDto.email })
+
+    if (emailExists) throw new ConflictException('Email already exists')
+
+    const user = new this.userModel({...createUserDto, createdAt: new Date()})
+
+    try {
+      await user.save()
+    } catch (error) {
+      return error
+    }
+    return user
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userModel.find()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async update(id: string, updateUserDto: CreateUserDto) {
+
+    const user = await this.userModel.findById(id)
+
+    try {
+      user.set(updateUserDto)
+      await user.save()
+      return user
+    } catch (error) {
+      return error
+    }
+
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOne(id: string) {
+    const user = await this.userModel.findById(id)
+
+    if (!user) throw new NotFoundException('User not found')
+    return user
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async delete(id: string) {
+    console.log(id)
+    return await this.userModel.findByIdAndDelete(id)
+
   }
 }
