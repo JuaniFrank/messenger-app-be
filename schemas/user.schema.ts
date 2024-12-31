249b07d388e210774377f48fbadff0723337434b
Juan I. Frank <juanfrank.dev@gmail.com>
2024-12-31 00:14:48 -0300

    feat: update user schema

--- schemas/user.schema.ts
+++ schemas/user.schema.ts
@@ -6,11 +6,26 @@ export type UserSchemaType = HydratedDocument<User>;
 
 @Schema()
 export class User {
-  @Prop({required: true, unique: true})
-  email: string;
+    @Prop({ required: true, unique: true })
+    email: string;
 
-  @Prop({required: true})
-  password: string;
+    @Prop({ required: true })
+    password: string;
+
+    @Prop()
+    firstName: string;
+
+    @Prop()
+    lastName: string;
+
+    @Prop()
+    username: string;
+
+    @Prop()
+    phoneNumber: string;
+
+    @Prop()
+    createdAt: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
