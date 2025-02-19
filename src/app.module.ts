6391520f45398eeebeb71c50bb4935b694a1c200
Juan I. Frank <juanfrank.dev@gmail.com>
2025-02-19 17:22:57 -0300

    fix: mongo db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, UserModule, MongooseModule.forRoot('mongodb://localhost:27017')],
+  imports: [AuthModule, UserModule, MongooseModule.forRoot('mongodb://root:root123@localhost:27017/miapp?authSource=admin')],
   controllers: [],
   providers: [],
 
 })
 export class AppModule {}
+
+
+
