98d080b0abf57d438b118edaa15f6638dc9504f9
Juan I. Frank <juanfrank.dev@gmail.com>
2025-05-06 00:02:57 -0300

    feat: refactor create user DTO

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,26 +1,66 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsEmail, IsOptional, IsString, IsNumber, IsDateString, IsPhoneNumber, IsObject } from 'class-validator';
 
 export class CreateUserDto {
-    @IsEmail()
-    @IsNotEmpty()
-    email: string;
+  @IsEmail()
+  email: string;
 
-    @IsNotEmpty()
-    password: string;
+  @IsString()
+  password: string;
 
-    @IsOptional()
-    @IsString()
-    firstName?: string;
+  @IsOptional()
+  @IsString()
+  firstName?: string;
 
-    @IsOptional()
-    @IsString()
-    lastName?: string;
+  @IsOptional()
+  @IsString()
+  lastName?: string;
 
-    @IsOptional()
-    @IsString()
-    username?: string;
+  @IsOptional()
+  @IsString()
+  maidenName?: string;
 
-    @IsOptional()
-    @IsString()
-    phoneNumber?: string;
+  @IsOptional()
+  @IsNumber()
+  age?: number;
+
+  @IsOptional()
+  @IsString()
+  gender?: string;
+
+  @IsOptional()
+  @IsPhoneNumber()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  username?: string;
+
+  @IsOptional()
+  @IsDateString()
+  birthDate?: string;
+
+  @IsOptional()
+  @IsString()
+  image?: string;
+
+  @IsOptional()
+  @IsObject()
+  address?: {
+    address?: string;
+    city?: string;
+    state?: string;
+    country?: string;
+  };
+
+  @IsOptional()
+  @IsObject()
+  company?: {
+    department?: string;
+    name?: string;
+    title?: string;
+  };
+
+  @IsOptional()
+  @IsString()
+  role?: string;
 }
